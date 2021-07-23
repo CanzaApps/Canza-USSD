af5a12c79654e0105e386bd02d3f5dd08e1dfc91
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-23 21:29:32 +0100

    project setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ussdRouter = require("./index");
+const ussdRouter = require("./services/ussd");
 const dotenv = require("dotenv").config();
 
 const app = express();
