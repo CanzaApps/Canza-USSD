de03bd369997e620ae7c6df790ac0cb7e7529b9b
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-24 17:02:36 +0100

    updated

--- test.js
+++ test.js
@@ -8,10 +8,12 @@ const alfatores = process.env.ALFAJORES;
 const kit = ContractKit.newKit(alfatores);
 // console.log(kit);
 
-const createAccount = async () => {
-    const 
-}
+// const createAccount = async () => {
+//     const 
+// }
 const getBalance = async (account) => {
     const balance = await kit.getTotalBalance(account);
     console.log(balance);
 }
+getBalance("0x130f747511d3581abc46654dd5f3d1b7910242d5")
+// getBalance("0xe650CFaCBCBa96D0535CFf72e7B7D42097A40Afa")
